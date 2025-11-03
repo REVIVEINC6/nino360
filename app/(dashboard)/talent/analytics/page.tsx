5d0d60b7e3f3b279d22ac5080b06aa95cea615bb
Pratapbp <210496551+pratapbp@users.noreply.github.com>
2025-11-03 09:49:22 -0500

    Degraded

--- app/(dashboard)/talent/analytics/page.tsx
+++
@@ -1,5 +0,0 @@
-import { TalentAnalyticsDashboard } from "@/components/talent/talent-analytics-dashboard"
-
-export default async function AnalyticsPage() {
-  return <TalentAnalyticsDashboard />
-}
